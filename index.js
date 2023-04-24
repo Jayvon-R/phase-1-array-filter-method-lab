4dc90e7820391c28360c1eb911861d2c102925fa
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-04-23 20:31:40 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(a, b) {
+    return a.filter(a => a.toLowerCase() === b.toLowerCase())
+}
+
+function fuzzyMatch(a, b) {
+    return a.filter(a => a.charAt(0) === b.charAt(0))
+}
+
+const matchName = (a, b) => a.filter(a => a.name === b)
